eecd74c10fb8f8ccad9646f5c26ba8c58de8bce3
Albert Cervera i Areny <albert@nan-tic.com>
2013-05-08 16:35:20 +0200

    Add encoding utf8 to setup.py to avoid crash.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# encoding: utf-8
 #This file is part jasper_reports module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
