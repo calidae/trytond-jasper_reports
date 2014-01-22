443779138b5887122440dba79d3ed331f0d63b83
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-01-22 18:12:47 +0100

    Use report id for storing cache as name can colide

--- jasper.py
+++ jasper.py
@@ -49,7 +49,7 @@ class JasperReport(Report):
 
     @classmethod
     def get_report_file(cls, report, path=None):
-        cache_path = cls._get_report_file_cache.get(report.name)
+        cache_path = cls._get_report_file_cache.get(report.id)
         if cache_path is not None:
             return cache_path
 
@@ -116,7 +116,7 @@ class JasperReport(Report):
             pfile = os.path.join(path, '%s_%s.properties' % (
                     basename, lang.lower()))
             cls.write_properties(pfile, p)
-        cls._get_report_file_cache.set(report.name, jrxml_path)
+        cls._get_report_file_cache.set(report.id, jrxml_path)
         return jrxml_path
 
     @classmethod
