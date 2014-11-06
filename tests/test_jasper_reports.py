58aecf95d33fe43c0b4d041880b9d63bf173c085
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-06 13:49:14 +0100

    Cleanup tests

--- tests/test_jasper_reports.py
+++ tests/test_jasper_reports.py
@@ -1,43 +1,28 @@
 #!/usr/bin/env python
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
-
-import sys, os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class JasperReportsTestCase(unittest.TestCase):
-    '''
-    Test JasperReports module.
-    '''
+    'Test JasperReports module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('jasper_reports')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('jasper_reports')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
+
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             JasperReportsTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
