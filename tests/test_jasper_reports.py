fa29733406a1fe69466fde756ca9333ebedb4eb5
resteve <resteve@zikzakmedia.com>
2015-12-01 11:09:44 +0100

    Tests

--- tests/test_jasper_reports.py
+++ tests/test_jasper_reports.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python
-# This file is part of Tryton.  The COPYRIGHT file at the top level of
-# this repository contains the full copyright notices and license terms.
+# This file is part of the jasper_reports module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class JasperReportsTestCase(unittest.TestCase):
-    'Test JasperReports module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('jasper_reports')
-
-    def test0005views(self):
-        'Test views'
-        test_view('jasper_reports')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class JasperReportsTestCase(ModuleTestCase):
+    'Test Jasper Reports module'
+    module = 'jasper_reports'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            JasperReportsTestCase))
+        JasperReportsTestCase))
     return suite
