78917522a28028fb921461ab598aa2405fe92bc3
jmartin <jmartin@zikzakmedia.com>
2016-02-08 15:09:14 +0100

    Takes the name of the report from the first part of the splitted report_name instead of the first character less the last seven

--- jasper.py
+++ jasper.py
@@ -87,7 +87,7 @@ class JasperReport(Report):
             for subreport in subreports:
                 sreport = subreport.split('"')
                 report_fname = sreport[1]
-                report_name = report_fname[:-7]  # .jasper
+                report_name = report_fname.split('.')[0]
                 ActionReport = Pool().get('ir.action.report')
 
                 report_actions = ActionReport.search([
