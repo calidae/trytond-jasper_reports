ce9c0fc60091dfd58ab8db80028c09a65ebbaa37
C?dric Krier <ced@b2ck.com>
2018-01-09 11:00:59 +0100

    Remove volumes from drone and fix setup for tests

--- setup.py
+++ setup.py
@@ -43,14 +43,14 @@ major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
 
-requires = []
+requires = ['PyPDF2', 'lxml']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         prefix = MODULE2PREFIX.get(dep, 'trytond')
         requires.append(get_require_version('%s_%s' % (prefix, dep)))
 requires.append(get_require_version('trytond'))
 
-tests_require = [get_require_version('proteus')]
+tests_require = []
 dependency_links = []
 if minor_version % 2:
     # Add development index for testing with proteus
@@ -117,7 +117,4 @@ setup(name='%s_%s' % (PREFIX, MODULE),
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
     use_2to3=True,
-    convert_2to3_doctests=[
-        'tests/scenario_jasper_reports.rst',
-        ],
     )
